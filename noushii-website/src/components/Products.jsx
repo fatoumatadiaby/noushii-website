cbd3dbc77e7596d21e1d141d02fe9f8fcc881562
fatoumatadiaby <fatimad5293@gmail.com>
2021-03-11 22:59:29 -0500

    created a product file and added a grid component

---
+++ noushii-website/src/components/Products.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Grid from '@material-ui/core'
+
+const Products = () => {
+    <main>
+        <Grid container justify="center" spacing= {4}>
+       </Grid>
+    </main>
+}
