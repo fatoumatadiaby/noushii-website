319d8c22288aab9ff19365eb73b05fc6d0f3a379
fatoumatadiaby <fatimad5293@gmail.com>
2021-03-11 23:10:49 -0500

    card content
    styled and created card content along with fixing bug issues

--- noushii-website/src/components/Product/Product/Product.jsx
+++ noushii-website/src/components/Product/Product/Product.jsx
@@ -11,10 +11,32 @@ import { AddShoppingCart } from "@material-ui/icons";
 
 const Product = () => {
     return (
-        <div>
-            
-        </div>
-    )
+      <div>
+        <Card className={classes.root}>
+          <CardMedia
+            className={classes.media}
+            image={product.media.source}
+            title={product.name}
+          />
+          <CardContent>
+            <div className={classes.cardContent}>
+              <Typography gutterBottom variant="h5" component="h2">
+                {product.name}
+              </Typography>
+              <Typography gutterBottom variant="h5" component="h2">
+                ${product.price.formatted}
+              </Typography>
+            </div>
+            <Typography
+              dangerouslySetInnerHTML={{ __html: product.description }}
+              variant="body2"
+              color="textSecondary"
+              component="p"
+            />
+          </CardContent>
+        </Card>
+      </div>
+    );
 }
 
 export default Product
