64e4adcc27a118c5e4eb77ce99856564c6a88973
fatoumatadiaby <fatimad5293@gmail.com>
2021-03-11 23:03:36 -0500

    map products in grid to show product list

--- noushii-website/src/components/Products.jsx
+++ noushii-website/src/components/Products.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import Grid from '@material-ui/core'
 
+const products = [
+    {id: 1, name:'swimsuitA', description: 'black zip up'}
+    {id: 2, name:'swimsuitB', description: 'black strings'}
+    {id: 3, name:'swimsuitC', description: 'black two piece'}
+]
 const Products = () => {
     <main>
         <Grid container justify="center" spacing= {4}>
+            {products.map((product) => {
+                
+            }}
        </Grid>
     </main>
 }
